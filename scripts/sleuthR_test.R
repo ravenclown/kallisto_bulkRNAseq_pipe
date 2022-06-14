b04a67bbd9334286cf1edca0470bfb80742fbfe3
Mert Yıldız <58143354+ravenclown@users.noreply.github.com>
2022-06-14 16:18:46 +0300

    Update sleuthR_test.R

--- scripts/sleuthR_test.R
+++ scripts/sleuthR_test.R
@@ -6,13 +6,13 @@ library("biomaRt")
 library("dplyr")
 #arguments
 args = commandArgs(trailingOnly=TRUE)
-base_dir <- "~/Downloads/kallisto_bulkRNAseq_pipe-main/"
-sample_type <- "sample_conditions.txt"
+base_dir <- args[0]
+condition <- args[1]
 setwd(base_dir)
 #get sample id and directories
 sample_id <- dir(file.path(base_dir,"quant"))
 kal_dirs <- sapply(sample_id, function(id) file.path(base_dir,"quant", id))
-condition <- read.table(sample_type, header = TRUE, stringsAsFactors=FALSE) 
+condition <- read.table(condition, header = TRUE, stringsAsFactors=FALSE) 
 #get gene data from ensemble biomart
 mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",dataset = "hsapiens_gene_ensembl",host = 'ensembl.org')
 t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id","external_gene_name"), mart = mart)
